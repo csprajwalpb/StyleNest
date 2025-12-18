fa5390a36bc8509ac2f2c46eef0726b7666014ec
Sebastian Davis <sebastiandavis109@gmail.com>
2025-12-18 21:13:40 +0530

    admin endpoint port

--- backend/index.js
+++ backend/index.js
@@ -206,3 +206,42 @@ app.post('/getcart', fetchuser, async (req, res) => {
   res.json(userData.cartData);
 
 })
+
+
+// Create an endpoint for adding products using admin panel
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  }
+  else { id = 1; }
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    description: req.body.description,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
+  await product.save();
+  console.log("Saved");
+  res.json({ success: true, name: req.body.name })
+});
+
+
+// Create an endpoint for removing products using admin panel
+app.post("/removeproduct", async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  console.log("Removed");
+  res.json({ success: true, name: req.body.name })
+});
+
+// Starting Express Server
+app.listen(port, (error) => {
+  if (!error) console.log("Server Running on port " + port);
+  else console.log("Error : ", error);
+});
