3d8241a725481375dfddeb5e40bb7ed96318b98d
dhanyachintu <chintudhanyass69410@gmail.com>
2025-12-18 12:58:43 +0530

    Add endpoints and category-based related product recommendations.

--- backend/index.js
+++ backend/index.js
@@ -156,4 +156,20 @@ app.get("/newcollections", async (req, res) => {
   console.log("New Collections");
   res.send(arr);
 });
+// endpoint for getting womens products data
+app.get("/popularinwomen", async (req, res) => {
+  let products = await Product.find({ category: "women" });
+  let arr = products.splice(0, 4);
+  console.log("Popular In Women");
+  res.send(arr);
+});
+
+// endpoint for getting womens products data
+app.post("/relatedproducts", async (req, res) => {
+  console.log("Related Products");
+  const {category} = req.body;
+  const products = await Product.find({ category });
+  const arr = products.slice(0, 4);
+  res.send(arr);
+});
 
