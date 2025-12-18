b9ed6b01067d4759e4e90a3e1d8527e936a7e520
NikhilAMagaji <nikhilamagaji@gmail.com>
2025-12-18 14:57:31 +0530

    updated index.js endpoint to save remove and get cartdata

--- backend/index.js
+++ backend/index.js
@@ -173,3 +173,36 @@ app.post("/relatedproducts", async (req, res) => {
   res.send(arr);
 });
 
+
+
+
+
+// Create an endpoint for saving the product in cart
+app.post('/addtocart', fetchuser, async (req, res) => {
+  console.log("Add Cart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  userData.cartData[req.body.itemId] += 1;
+  await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+  res.send("Added")
+})
+
+
+// Create an endpoint for removing the product in cart
+app.post('/removefromcart', fetchuser, async (req, res) => {
+  console.log("Remove Cart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  if (userData.cartData[req.body.itemId] != 0) {
+    userData.cartData[req.body.itemId] -= 1;
+  }
+  await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
+  res.send("Removed");
+})
+
+
+// Create an endpoint for getting cartdata of user
+app.post('/getcart', fetchuser, async (req, res) => {
+  console.log("Get Cart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  res.json(userData.cartData);
+
+})
