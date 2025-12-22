eebead524d0ac3cab16adb2749fe23133d71c331
Manojnm24 <manoj80204@gmail.com>
2025-12-22 19:09:54 +0530

    updated navbar jsx

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     <div className='nav'>
       <Link to='/' onClick={()=>{setMenu("shop")}} style={{ textDecoration: 'none' }} className="nav-logo">
         <img src={logo} alt="logo" />
-        <p>StyleNest</p>
+        <p>STYLENEST</p>
       </Link>
       <img onClick={dropdown_toggle} className='nav-dropdown' src={nav_dropdown} alt="" />
       <ul ref={menuRef} className="nav-menu">
